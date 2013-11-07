3e17af5c643ac1b497e41bc1a0847ab9c0abcd3a
rpthis! <mprado85@gmail.com>
2013-11-07 09:54:34 -0800

    Adding Hellow World App

---
+++ A_HelloWorld/HelloController.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marmarmar
+ * Date: 11/7/13
+ * Time: 9:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function HelloController($scope)
+{
+    $scope.greeting = {text:"Hello World"};
+}
