f133190aa4dab1823dcc65f373aa8e2d5961c263
rpthis! <mprado85@gmail.com>
2013-11-08 14:19:57 -0800

    Adding list example

---
+++ A_2_List/CartController.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marmarmar
+ * Date: 11/8/13
+ * Time: 1:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+function CartController($scope)
+{
+    $scope.items = [
+
+        {title:"item one", price:10},
+        {title:"item two", price:45}
+    ]
+
+
+}
