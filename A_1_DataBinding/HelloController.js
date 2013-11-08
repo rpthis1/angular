456cf758787921e07999e66009e536c4388bb454
rpthis! <mprado85@gmail.com>
2013-11-07 21:17:26 -0800

    adding A_1_DataBinding App

---
+++ A_1_DataBinding/HelloController.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marmarmar
+ * Date: 11/7/13
+ * Time: 9:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function HelloController($scope)
+{
+    $scope.greeting = {text:"Hello There"};
+}
+
